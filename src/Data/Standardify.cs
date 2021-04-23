a0532750f1543def3469d089d1dcb32f54b20069
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2021-04-23 18:25:15 -0300

    Add files via upload

---
+++ src/Data/Standardify.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quasarStack.Data
+{
+    public static class Standardify
+    {
+        public static int[] GenInt(int xfrom, int to)
+        {
+            List<int> FinalList = new List<int>();
+            while (xfrom < to)
+            {
+                FinalList.Add(xfrom);
+                xfrom++;
+            }
+            while (xfrom > to)
+            {
+                FinalList.Add(xfrom);
+                xfrom--;
+            }
+            if (xfrom == to) { FinalList.Add(to); }
+            return FinalList.ToArray();
+        }
+
+        public static string[] GenStandard(string type, int start, int stop)
+        {
+
+            switch(type)
+            {
+                case "month":
+                    return InternalGetMonthStandard(start, stop);
+
+                case "week":
+                    return InternalGetWeekStandard(start, stop);
+            }
+
+            General.ReportError(General.ERR_DATAMAKE);
+            return null;
+            
+        }
+
+        static string[] InternalGetMonthStandard(int start, int stop)
+        {
+            int[] monthNumList = GenInt(start, stop);
+            List<string> FinalList = new List<string>();
+            int Tester = 0;
+
+            foreach (int m in monthNumList)
+            {
+                Tester++;
+                if (Tester > 12) { Tester = 1; }
+                switch (Tester)
+                {
+                    case 1:
+                        FinalList.Add("January");
+                        break;
+
+                    case 2:
+                        FinalList.Add("February");
+                        break;
+
+                    case 3:
+                        FinalList.Add("March");
+                        break;
+
+                    case 4:
+                        FinalList.Add("April");
+                        break;
+
+                    case 5:
+                        FinalList.Add("May");
+                        break;
+
+                    case 6:
+                        FinalList.Add("June");
+                        break;
+
+                    case 7:
+                        FinalList.Add("July");
+                        break;
+
+                    case 8:
+                        FinalList.Add("August");
+                        break;
+
+                    case 9:
+                        FinalList.Add("September");
+                        break;
+
+                    case 10:
+                        FinalList.Add("October");
+                        break;
+
+                    case 11:
+                        FinalList.Add("November");
+                        break;
+
+                    case 12:
+                        FinalList.Add("December");
+                        break;
+
+                    default:
+                        General.ReportError(General.ERR_LOGIC);
+                        break;
+                }
+            }
+
+            return FinalList.ToArray();
+
+        }
+
+        static string[] InternalGetWeekStandard(int start, int stop)
+        {
+            int[] weekNumList = GenInt(start, stop);
+            List<string> FinalList = new List<string>();
+            int Tester = 0;
+
+            foreach (int m in weekNumList)
+            {
+                Tester++;
+                if (Tester > 7) { Tester = 1; }
+                switch (Tester)
+                {
+                    case 1:
+                        FinalList.Add("Sunday");
+                        break;
+
+                    case 2:
+                        FinalList.Add("Monday");
+                        break;
+
+                    case 3:
+                        FinalList.Add("Tuesday");
+                        break;
+
+                    case 4:
+                        FinalList.Add("Wednesday");
+                        break;
+
+                    case 5:
+                        FinalList.Add("Thursday");
+                        break;
+
+                    case 6:
+                        FinalList.Add("Friday");
+                        break;
+
+                    case 7:
+                        FinalList.Add("Saturday");
+                        break;
+
+                    default:
+                        General.ReportError(General.ERR_LOGIC);
+                        break;
+                }
+            }
+
+            return FinalList.ToArray();
+
+        }
+    }
+}
---
+++ src/General.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace quasarStack
+{
+    public static class QuasarProductInfo
+    {
+        public static string Name = "quasarStack";
+        public static string Version = "[Alpha] [ReleaseVer] 0.3"; //LAST UPDATE DAY 19 04/2021
+    }
+    public static class General
+    {
+        public static string NETWORK_IP_API = "api.ipify.org";
+
+        public static List<string> QuasarStackGeneralLogRaw = new List<string>();
+        public static List<string> QuasarStackErrorLogRaw = new List<string>();
+
+        public static string ERR_CONVERSION = "[MEDIAN] An error occurred while performing the data conversion, " +
+        "review the code syntax, the entered values or the execution context.";
+        public static string ERR_LOGIC = "[COMMON] An undetermined logic error has occurred, " +
+        "review the logic used or the syntax implemented. If there's no visible problem in the runtime ignore the warning.";
+        public static string ERR_DATAMAKE = "[SEVERE] An undetermined Data Creation error has occurred.";
+        public static string ERR_MATH = "[MEDIAN] A mathematical error has occurred, please review the implemented logic";
+        public static string ERR_NETWORK = "[MEDIAN] A connection error has occurred, check that the source device is connected to " +
+        "the internet and review the network, firewall and antivirus settings.";
+        public static string ERR_NETWORK_SERVER = "[COMMON] A connection error has occurred, the target is inaccessible. Please try again.";
+        public static string ERR_NETWORK_SYSTEM_INFO = "[MEDIAN] An error occurred when trying to find information about the local network, " +
+        "check the program logic, its permissions and if the system is properly configured.";
+        public static string ERR_UNKNOWN = "[SEVERE] An unknown error was detected while the program was running.";
+
+        public static string WARN_NOHTTP_RESPONSE = "[WARNING] No HTTP response.";
+
+        public static void ReportError(string error)
+        {
+            QuasarStackErrorLogRaw.Add(DateTime.Now + " " + error);
+            QuasarStackGeneralLogRaw.Add(DateTime.Now + " " + error);
+        }
+
+        public static string GlobalErrorString()
+        {
+            string finalString = "";
+            foreach (string err in QuasarStackErrorLogRaw) 
+            { finalString += err + "\n"; }
+            return finalString;
+        }
+    }
+}
